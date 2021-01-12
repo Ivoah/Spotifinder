004d68c6def37d7b8ea26661d1f442bbda24c044
Noah Rosamilia <ivoahivoah@gmail.com>
2021-01-12 15:35:30 -0500

    Begin implementing UI

--- src/main/scala/Spotifinder.scala
+++ src/main/scala/Spotifinder.scala
@@ -1,30 +1,91 @@
+import java.awt.Image
+import java.net.URL
+import javax.swing.ImageIcon
 import javax.swing.border.EmptyBorder
+import scala.io.Source
 import scala.swing._
 import scala.swing.event._
 
 object Spotifinder extends MainFrame with App {
-  val emptyBorder = new EmptyBorder(0, 0, 0, 0)
+  def _with[A](source: Source, fn: Source => A): A = {
+    val result = fn(source)
+    source.close()
+    result
+  }
 
-  val ids = Seq("noahrosamilia", "1289823125")
+  val Array(client_id, client_secret) = _with(Source.fromResource("credentials.txt"), _.getLines.next.split(':'))
+  val api = Spotify(client_id, client_secret)
 
-  val lists = Seq(
-    new ListView(Seq("Noah Rosamilia", "Emma Brower", "Allison Kay")) {
-      println(listData)
+  val users = _with(Source.fromResource("users.txt"), _.getLines.map(api.User).toSeq)
 
-      selection.intervalMode = ListView.IntervalMode.Single
-      selection.reactions += {
-        case ListSelectionChanged(source, range, live) if !live =>
-          println(range)
-          println(selection.items)
-          println(selection.leadIndex, selection.anchorIndex)
+  val privatePlaylists = new Object {
+    override def toString: String = "Private playlists"
+    val playlists: Seq[api.Playlist] = _with(Source.fromResource("privatePlaylists.txt"), _.getLines.map(api.Playlist.fromId).toSeq)
+  }
+
+  val usersList = new ListView(privatePlaylists +: users) {
+    selection.intervalMode = ListView.IntervalMode.Single
+    selection.reactions += {
+      case ListSelectionChanged(source, range, live) if !live && selection.items.nonEmpty =>
+        playlistsList.listData = selection.items.head.playlists
+        songsList.listData = Seq()
+        infoPanel.track = None
+    }
+  }
+  val playlistsList = new ListView[api.Playlist]() {
+    selection.intervalMode = ListView.IntervalMode.Single
+    selection.reactions += {
+      case ListSelectionChanged(source, range, live) if !live && selection.items.nonEmpty =>
+        songsList.listData = selection.items.head.tracks
+        infoPanel.track = None
+    }
+  }
+  val songsList = new ListView[api.PlaylistItem]() {
+    selection.intervalMode = ListView.IntervalMode.Single
+    selection.reactions += {
+      case ListSelectionChanged(source, range, live) if !live && selection.items.nonEmpty =>
+        infoPanel.track = selection.items.head
+    }
+  }
+  val infoPanel = new BoxPanel(Orientation.Vertical) {
+    private var _track: Option[api.PlaylistItem] = None
+    def track: Option[api.PlaylistItem] = _track
+    def track_=(new_track: Option[api.PlaylistItem]): Unit = {
+      _track = new_track
+      _track match {
+        case Some(track) =>
+          val icon = new ImageIcon(new URL(track.track.album.artwork))
+          labels("artwork").icon = new ImageIcon(icon.getImage.getScaledInstance(250, 250, Image.SCALE_SMOOTH))
+          labels("name").text = track.track.name
+          labels("album").text = track.track.album.name
+          labels("artists").text = track.track.artists.map(_.name).mkString(", ")
+          labels("added_by").text = track.added_by.name
+          labels("added_at").text = track.added_at.toString
+        case None =>
+          labels("artwork").icon = null
+          labels.values.foreach(_.text = "")
       }
-    },
-    new ListView(Seq("Sunrise", "Driving", "I thought I hated country")),
-    new ListView(Seq("Monsters of the North", "Better Place", "Si j'osais")),
-    new Panel {}
-  )
+    }
+    def track_=(new_track: api.PlaylistItem): Unit = {
+      track = Some(new_track)
+    }
+
+    val labels = collection.immutable.ListMap(
+      "artwork" -> new Label {
+        preferredSize = new Dimension(250, 250)
+      },
+      "name" -> new Label,
+      "album" -> new Label,
+      "artists" -> new Label,
+      "added_by" -> new Label,
+      "added_at" -> new Label
+    )
+
+    contents ++= labels.values
+  }
 
   def makeSplitPanes(lists: Seq[Component]): SplitPane = {
+    val emptyBorder = new EmptyBorder(0, 0, 0, 0)
     lists match {
       case second_last::last::Nil => new SplitPane(Orientation.Vertical,
         new ScrollPane(second_last) {border = emptyBorder},
@@ -37,9 +98,13 @@ object Spotifinder extends MainFrame with App {
     }
   }
 
-  contents = makeSplitPanes(lists)
+  val searchBar = new TextField
 
-  pack()
+  contents = new BorderPanel {
+    layout(searchBar) = BorderPanel.Position.North
+    layout(makeSplitPanes(Seq(usersList, playlistsList, songsList, infoPanel))) = BorderPanel.Position.Center
+  }
+
   centerOnScreen()
   open()
 }
--- src/main/scala/Spotify.scala
+++ src/main/scala/Spotify.scala
@@ -3,8 +3,6 @@ import play.api.libs.functional.syntax.toFunctionalBuilderOps
 import java.util.Date
 import play.api.libs.json._
 
-import java.text.SimpleDateFormat
-
 case class Spotify(client_id: String, client_secret: String) {
   private implicit val dateReads: Reads[Date] = Reads.dateReads("yyyy-MM-dd'T'HH:mm:ss'Z'")
 
@@ -38,14 +36,22 @@ case class Spotify(client_id: String, client_secret: String) {
 
     lazy val name: String = (jsonObj \ "display_name").as[String]
     lazy val playlists: Seq[Playlist] = getPlaylists()
+
+    override def toString: String = name
   }
 
   private implicit val artistReads: Reads[Artist] = Json.reads[Artist]
   case class Artist(id: String, name: String)
 
   private implicit val playlistReads: Reads[Playlist] = Json.reads[Playlist]
+  object Playlist {
+    def fromId(id: String): Playlist = {
+      val jsonObj = Json.parse(get(s"https://api.spotify.com/v1/playlists/$id?fields=id%2Cname%2Cdescription"))
+      jsonObj.as[Playlist]
+    }
+  }
   case class Playlist(id: String, name: String, description: String) {
-    private def getTracks(url: String = s"https://api.spotify.com/v1/playlists/$id/tracks?fields=items(added_by.id%2Cadded_at%2Ctrack(id%2Cname%2Calbum(name%2Cartists(id%2Cname))))%2Cnext"): Seq[PlaylistItem] = {
+    private def getTracks(url: String = s"https://api.spotify.com/v1/playlists/$id/tracks?items(added_by.id%2Cadded_at%2Ctrack(id%2Cname%2Calbum(id%2Cmages%2Cname)%2Cartists(id%2Cname)))%2Cnext"): Seq[PlaylistItem] = {
       val jsonObj = Json.parse(get(url))
       val tracks = (jsonObj \ "items").as[Seq[PlaylistItem]]
       (jsonObj \ "next").asOpt[String] match {
@@ -54,23 +60,30 @@ case class Spotify(client_id: String, client_secret: String) {
       }
     }
     lazy val tracks: Seq[PlaylistItem] = getTracks()
+
+    override def toString: String = name
   }
 
-  private implicit val trackReads: Reads[Track] = (
+  private implicit val albumReads: Reads[Album] = (
     (JsPath \ "id").read[String] and
       (JsPath \ "name").read[String] and
-      (JsPath \ "album" \ "name").read[String] and
-      (JsPath \ "album" \ "artists").read[Seq[Artist]]
-    )(Track.apply _)
-  case class Track(id: String, name: String, album: String, artists: Seq[Artist])
+      (JsPath \ "images" \ 0 \ "url").read[String]
+    )(Album.apply _)
+  case class Album(id: String, name: String, artwork: String)
+
+  private implicit val trackReads: Reads[Track] = Json.reads[Track]
+  case class Track(id: String, name: String, album: Album, artists: Seq[Artist])
 
   private implicit val playlistItemReads: Reads[PlaylistItem] = Json.reads[PlaylistItem]
-  case class PlaylistItem(track: Track, added_by: User, added_at: Date)
+  case class PlaylistItem(track: Track, added_by: User, added_at: Date) {
+    override def toString: String = track.name
+  }
 }
 
 object Spotify {
   def main(args: Array[String]): Unit = {
-
+    val CLIENT_ID = "f27ceb8f09e54d8286ad2fe0582aaad5"
+    val CLIENT_SECRET = "24297f6dd14a457292dc5fd0111c416b"
 
     val api = Spotify(CLIENT_ID, CLIENT_SECRET)
     val user = api.User("noahrosamilia")
